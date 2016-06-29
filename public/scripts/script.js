2e584171feca77a30f5325dfc7af7a3b71a1b447
devjanaprime <dev@primeacademy.io>
2016-06-28 21:42:15 -0500

    added SQL watchlist save

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,10 +4,32 @@ var myApp = angular.module( 'myApp', [] );
 myApp.controller( 'tuesdayController',[ '$scope', '$http', function( $scope, $http ){
   // global list of movie searches
   $scope.everySearch=[];
+  // header shown
+  $scope.currentHeader='';
+
+  // add a movie to favorites in db
+  $scope.addFavorite = function( index ){
+    console.log( 'in addFavorite: ' + $scope.everySearch[ index ].title );
+    // add to favorites db
+    var objectToSend ={
+      title: $scope.everySearch[ index ].title,
+      year: $scope.everySearch[ index ].year,
+      director: $scope.everySearch[ index ].director,
+      posterURL: $scope.everySearch[ index ].posterURL,
+    };
+    $http({
+      method: 'POST',
+      url: '/addFavorite',
+      data: objectToSend
+    }).then( function(response){
+      console.log( 'back from adding: ' +  objectToSend.title );
+    }); //end http, then
+    // remove from current searches list
+    $scope.everySearch.splice( index );
+  };
 
   // test get user input
   $scope.movieSearch = function(){
-
     console.log( 'in movieSearch: ' + $scope.movieIn );
     // assemble API URL
     var apiURL = 'https://www.omdbapi.com/?t=' + $scope.movieIn + '&type=movie&tomatoes=true%27';
--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
 var path = require('path');
+var pg = require('pg');
+
+// setup connection info for SQL db
+if( process.env.DATABASE_URL ){
+    pg.defaults.ssl=true;
+    connectionString = process.env.DATABASE_URL;
+} else {
+    connectionString = 'postgress://localhost:5432/movies';
+}
 
 // base url
 app.get( '/', function( req, res ){
@@ -12,6 +23,21 @@ app.set("port", (process.env.PORT || 5000));
 
 app.listen( app.get("port"), function(){
     console.log("listening on port", app.get("port"));
+});
+
+app.post( '/addFavorite', function( req, res ){
+  console.log( 'in addFavorite:' + req.body.title );
+  // add this movie to db
+  pg.connect( connectionString, function( err, client, done ){
+    if( err ){
+      console.log( err );
+      res.send( false );
+    } // end error
+    else{
+      client.query( 'INSERT INTO watchlist ( title, year, director, poster_url ) VALUES ( $1, $2, $3, $4 )', [ req.body.title, req.body.year, req.body.director, req.body.posterURL ] );
+      res.send( true );
+    } // end no error
+  }); // end connect
 });
 
 app.use( express.static( 'public' ) );
