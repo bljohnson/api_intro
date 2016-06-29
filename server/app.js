d4b2ff55bb0647348da2bfb1f52baca708115691
devjanaprime <dev@primeacademy.io>
2016-06-28 20:54:16 -0500

    setup for heroku deploy

--- server/app.js
+++ server/app.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = process.env.PORT || 3000;
+
 // base url
 app.get( '/', function( req, res ){
   console.log( 'in base url' );
   res.sendFile( path.resolve( 'views/index.html' ) );
 });
 
-// spin up server
-// app.listen( 8082, 'localhost', function( req, res ){
-//   console.log( 'server up on 8082' );
-// });
+app.set("port", (process.env.PORT || 5000));
 
-var server= app.listen( port, function(){
-    var port = server.address().port;
-    console.log( "Something is alive on port: " + port);
+app.listen( app.get("port"), function(){
+    console.log("listening on port", app.get("port"));
 });
 
-// static folder set to public
 app.use( express.static( 'public' ) );
